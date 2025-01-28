3ad2474a89cc5880b390e0b6bf9e85ce8d40b664
Giovanna Grangeiro <giigrangvanderlei1211@hotmail.com>
2025-01-28 12:43:44 -0300

    Domain model

---
+++ src/main/java/com/devsuperior/dslist/entities/Belonging.java
@@ -0,0 +1,54 @@
+package com.devsuperior.dslist.entities;
+
+import java.util.Objects;
+
+public class Belonging {
+	
+	private BelongingPK id = new BelongingPK();
+
+	private Integer position;
+	
+	public void setGame(Game game) {
+		id.setGame(game);
+	}
+	
+	public Game getGame() {
+		return id.getGame();
+	}
+	
+	public void setList (GameList list) {
+		id.setList(list);
+	}
+	
+	public GameList getList() {
+		return id.getList();
+	}
+	
+	public Integer getPosition() {
+		return position;
+	}
+
+	public void setPosition(Integer position) {
+		this.position = position;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, position);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Belonging other = (Belonging) obj;
+		return Objects.equals(id, other.id) && Objects.equals(position, other.position);
+	}
+	
+	
+
+}
